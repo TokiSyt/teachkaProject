7ed7049031621242e5410011b6e32b210c7fbfd0
TokiSyt <yuutokei30@gmail.com>
2025-09-18 15:46:43 +0200

    Updating name wheel and groups model

---
+++ apps/wheel/services/utils.py
@@ -0,0 +1,13 @@
+import random
+
+def choose_a_random_name(name_list, already_chosen_names):
+    remaining_names = [name for name in name_list if name not in already_chosen_names]
+    
+    if not remaining_names:
+        already_chosen_names = []
+        remaining_names = name_list
+        
+    new_choice = random.choice(remaining_names)
+    already_chosen_names.append(new_choice)
+    
+    return new_choice, already_chosen_names
