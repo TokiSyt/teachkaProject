c93dcf4758e4e684aca2df6ab42d03985b150652
TokiSyt <yuutokei30@gmail.com>
2025-12-03 17:08:53 +0100

    1 point gap fix in decimal grades

--- apps/grade_calculator/services/grade_calculator.py
+++ apps/grade_calculator/services/grade_calculator.py
@@ -1,4 +1,4 @@
-def grade_calculator(max_points: int, rounding_option: int) -> list[float]:
+def grade_calculator(max_points: float, rounding_option: int) -> list[float]:
     """
     Calculates grade thresholds based on max_points and rounding option.
 
@@ -11,9 +11,16 @@ def grade_calculator(max_points: int, rounding_option: int) -> list[float]:
         max_points * x for x in [1, 0.90, 0.89, 0.75, 0.74, 0.50, 0.49, 0.35, 0.34, 0]
     ]
     grades = [round(grade) for grade in grades]
-
+    
     for grade_index in range(len(grades) - 1):
         if grades[grade_index] == grades[grade_index + 1]:
             grades[grade_index] += 1 if rounding_option == 1 else 0.5
 
+    increment = 1 if rounding_option == 1 else 0.5
+    pairs = [(1, 2), (3, 4), (5, 6), (7, 8)]
+    for high, low in pairs:
+        if grades[high] - grades[low] >= 1:
+            grades[low] += increment
+    
+    grades[0] = max_points
     return grades
