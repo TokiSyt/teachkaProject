ba025adf7eb9dcab463fffb0264403cab9ee9e49
TokiSyt <yuutokei30@gmail.com>
2026-01-26 16:52:52 +0100

    Fix mobile, sidebar, and redirects

--- apps/group_divider/urls.py
+++ apps/group_divider/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GroupDividerHome
 
 urlpatterns = [
-    path("", GroupDividerHome.as_view(), name="group-diviver-home"),
+    path("", GroupDividerHome.as_view(), name="group-divider-home"),
 ]
